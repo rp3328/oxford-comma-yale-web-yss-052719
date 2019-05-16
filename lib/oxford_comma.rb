ebf1d61fb8f2dc63533e6c3c64b4052259eb2484
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-16 12:41:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
   len = array.size
+  com = ", "
+  two = " and "
+  last = ", and "
 
-  array[0..-1].join(",")
+  if len == 0
+    return ""
+  elseif len == 1
+    return array.join
+  elseif len == 2
+    return ""
+  array[0..-1].join(", ")
 end
